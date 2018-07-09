82b1ddb35814e8d4c4b17e12e950ce30c50b10a9
Connor Cashman <connorcashman@Connors-MacBook-Air.local>
2018-07-09 16:11:19 +0800

    Clean up diary

--- PropertyFinder/screens/diary.js
+++ PropertyFinder/screens/diary.js
@@ -7,15 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, Button,
-  TouchableOpacity, AppRegistry, TouchableHighlight, Modal, CameraRoll,
-  Dimensions, ScrollView} from 'react-native';
-
-import Share from 'react-native-share'
-import RNFetchBlob from 'react-native-fetch-blob'
-
-let styles
-const { width } = Dimensions.get('window')
+import {Platform, StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -38,118 +30,6 @@ class DiaryTextInput extends Component{
   }
 }
 
-AppRegistry.registerComponent('rncameraroll, () => Navigation');
-
-class PhotoBrowse extends Component<Props> {
-
-  static navigationOptions = {
-      title: 'Camera Roll'
-    }
-
-    state = {
-    modalVisible: false,
-    photos: [],
-    index: null
-  }
-
-  setIndex = (index) => {
-    if (index === this.state.index) {
-      index = null
-    }
-    this.setState({ index })
-  }
-
-  getPhotos = () => {
-    CameraRoll.getPhotos({
-      first: 4,
-      assetType: 'All' /*Change this line if we want only photos (currently gets video too)*/
-    })
-    .then(r => this.setState({ photos: r.edges }))
-  }
-
-  toggleModal = () => {
-    this.setState({ modalVisible: !this.state.modalVisible });
-  }
-
-  navigate = () => {
-      const { navigate } = this.props.navigation
-      navigate('ImageBrowser')
-    }
-
-    share = () => {
-        const image = this.state.photos[this.state.index].node.image.uri
-        RNFetchBlob.fs.readFile(image, 'base64')
-        .then((data) => {
-          let shareOptions = {
-            title: "My Day Photo",
-            message: "Check out this photo!",
-            url: `data:image/jpg;base64,${data}`,
-            subject: "Check out what I did today!"
-          }
-        }
-      }
-      render() {
-        return (
-          <View style={styles.container}>
-            <Text style={styles.welcome}>Welcome to React Native!</Text>
-            <Text style={styles.instructions}>Farking Christ, edit App.js</Text>
-            <Text style={styles.instructions}>{instructions}</Text>
-          </View>
-          <Button
-              title='Browse Images'
-              onPress={this.navigate}
-            />
-          <Modal
-              animationType={"slide"}
-              transparent={false}
-              visible={this.state.modalVisible}
-              onRequestClose={() => console.log('closed')}
-            >
-            <View style={styles.modalContainer}>
-                <Button
-                  title='Close'
-                  onPress={this.toggleModal}
-                />
-                <ScrollView
-                  contentContainerStyle={styles.scrollView}>
-                  {
-                    this.state.photos.map((p, i) => {
-                      return (
-                        <TouchableHighlight
-                          style={{opacity: i === this.state.index ? 0.5 : 1}}
-                          key={i}
-                          underlayColor='transparent'
-                          onPress={() => this.setIndex(i)}
-                        >
-                          <Image
-                            style={{
-                              width: width/3,
-                              height: width/3
-                            }}
-                            source={{uri: p.node.image.uri}}
-                          />
-                        </TouchableHighlight>
-                      )
-                 })
-               }
-               </ScrollView>
-                {
-                  this.state.index !== null  && (
-                    <View style={styles.shareButton}>
-                      <Button
-                          title='Share'
-                          onPress={this.share}
-                        />
-                    </View>
-                  )
-                }
-              </View>
-            </Modal>
-          </View>
-
-        );
-      }
-    }
 
 
 export default class Diary extends Component<Props> {
@@ -168,17 +48,13 @@ export default class Diary extends Component<Props> {
   render () {
     return (
       <View style={styles.container}>
-          <View style={styles.list}>
-              <TextInput
-                editable = {true}
-                autoFocus = {true}
-                blurOnSubmit = {false}
-                style = {{height: 80, width: 320, borderColor: 'blue', borderWidth: 1}}
-                onChangeText = {(text) => this.setState({text})}
-                value = {this.state.text}
-              />
-            <Image style={styles.backgroundImage} source={require('../res/cloud.png')}/>
-          </View>
+        <Image style={styles.backgroundImage} source={require('../res/cloud.png')}/>
+        <TextInput
+          blurOnSubmit = {false}
+          style = {{height: 80, width: 320, borderColor: 'blue', borderWidth: 1}}
+          onChangeText = {(text) => this.setState({text})}
+          value = {this.state.text}
+        />
         <View style={styles.diaryView}>
         <Button
           title = "Save Entry"
