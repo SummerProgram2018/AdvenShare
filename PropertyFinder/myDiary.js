bc8d3fa5cd1550bdbe215a92f197e3d47d400ffa
Ed Southall <edward.southall@uq.net.au>
2018-07-09 11:37:29 +1000

    Wrote initial code to hopefully navigate to camera roll from my diary

--- PropertyFinder/myDiary.js
+++ PropertyFinder/myDiary.js
@@ -7,7 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, AppRegistry, TouchableHighlight,
+Modal, Button, CameraRoll, Image, Dimensions, ScrollView} from 'react-native';
+
+import Share from 'react-native-share'
+import RNFetchBlob from 'react-native-fetch-blob'
+
+let styles
+const { width } = Dimensions.get('window')
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,8 +23,65 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const Navigation = StackNavigator({
+  App: { screen: App},
+  ImageBrowser: { screen: ImageBrowser}
+});
+
+AppRegistry.registerComponent('rncameraroll, () => Navigation');
+
+
+
 type Props = {};
 export default class App extends Component<Props> {
+
+  static navigationOptions = {
+      title: 'Camera Roll'
+    }
+
+    state = {
+    modalVisible: false,
+    photos: [],
+    index: null
+  }
+
+  setIndex = (index) => {
+    if (index === this.state.index) {
+      index = null
+    }
+    this.setState({ index })
+  }
+
+  getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 4,
+      assetType: 'All' /*Change this line if we want only photos (currently gets video too)*/
+    })
+    .then(r => this.setState({ photos: r.edges }))
+  }
+
+  toggleModal = () => {
+    this.setState({ modalVisible: !this.state.modalVisible });
+  }
+
+  navigate = () => {
+      const { navigate } = this.props.navigation
+      navigate('ImageBrowser')
+    }
+
+    share = () => {
+        const image = this.state.photos[this.state.index].node.image.uri
+        RNFetchBlob.fs.readFile(image, 'base64')
+        .then((data) => {
+          let shareOptions = {
+            title: "My Day Photo",
+            message: "Check out this photo!",
+            url: `data:image/jpg;base64,${data}`,
+            subject: "Check out what I did today!"
+          }
+        }
+      }
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,9 +89,65 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>Farking Christ, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+      <Button
+          title='Browse Images'
+          onPress={this.navigate}
+        />
+      <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => console.log('closed')}
+        >
+        <View style={styles.modalContainer}>
+            <Button
+              title='Close'
+              onPress={this.toggleModal}
+            />
+            <ScrollView
+              contentContainerStyle={styles.scrollView}>
+              {
+                this.state.photos.map((p, i) => {
+                  return (
+                    <TouchableHighlight
+                      style={{opacity: i === this.state.index ? 0.5 : 1}}
+                      key={i}
+                      underlayColor='transparent'
+                      onPress={() => this.setIndex(i)}
+                    >
+                      <Image
+                        style={{
+                          width: width/3,
+                          height: width/3
+                        }}
+                        source={{uri: p.node.image.uri}}
+                      />
+                    </TouchableHighlight>
+                  )
+             })
+           }
+           </ScrollView>
+            {
+              this.state.index !== null  && (
+                <View style={styles.shareButton}>
+                  <Button
+                      title='Share'
+                      onPress={this.share}
+                    />
+                </View>
+              )
+            }
+          </View>
+        </Modal>
+      </View>
+
     );
   }
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
